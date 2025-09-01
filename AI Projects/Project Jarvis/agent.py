10a22cd92db5b5dd956496cc5a32e0c03ee9cafc
japanlepenos <j.choladhorn@gmail.com>
2025-09-01 23:14:46 +0700

    Commit new changes

--- AI Projects/Project Jarvis/agent.py
+++ AI Projects/Project Jarvis/agent.py
@@ -13,10 +13,12 @@ class Assistant(Agent):
     def __init__(self) -> None:
         super().__init__(instructions="You are a helpful voice AI assistant.")
 
+
 async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
         llm=openai.realtime.RealtimeModel(
-            voice="coral"
+            voice="coral",
+            model="gpt-5",
         )
     )
 
@@ -32,6 +34,7 @@ async def entrypoint(ctx: agents.JobContext):
     await session.generate_reply(
         instructions="Greet the user and offer your assistance."
     )
+
 
 if __name__ == "__main__":
     agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
