32a15c8cb37266ba82f135951330eb5cd82a7439
japanlepenos <j.choladhorn@gmail.com>
2025-09-15 20:37:51 +0700

    Commit new changes

--- AI Projects/Project Jarvis/agent.py
+++ AI Projects/Project Jarvis/agent.py
@@ -1,17 +1,17 @@
 from dotenv import load_dotenv
-
 from livekit import agents
 from livekit.agents import AgentSession, Agent, RoomInputOptions
 from livekit.plugins import (
     openai,
     noise_cancellation,
 )
+from prompts.py import AGENT_INSTRUCTIONS, SESSION_INSTRUCTIONS
 
 load_dotenv()
 
 class Assistant(Agent):
     def __init__(self) -> None:
-        super().__init__(instructions="You are a helpful voice AI assistant.")
+        super().__init__(instructions=AGENT_INSTRUCTIONS)
 
 
 async def entrypoint(ctx: agents.JobContext):
@@ -26,13 +26,12 @@ async def entrypoint(ctx: agents.JobContext):
         room=ctx.room,
         agent=Assistant(),
         room_input_options=RoomInputOptions(
-            # For telephony applications, use `BVCTelephony` instead for best results
             noise_cancellation=noise_cancellation.BVC(),
         ),
     )
 
     await session.generate_reply(
-        instructions="Greet the user and offer your assistance."
+        instructions=SESSION_INSTRUCTIONS
     )
 
 
