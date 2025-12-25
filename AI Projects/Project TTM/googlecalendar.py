773d5329adf2643e57e30b5dda16948267c1a132
japanlepenos <j.choladhorn@gmail.com>
2025-12-25 17:41:49 +0700

    Put token.json in gitignore

---
+++ AI Projects/Project TTM/googlecalendar.py
@@ -0,0 +1,72 @@
+"""
+Docstring for googlecalendar
+
+Included Features:
+- Get upcoming events from the user's primary Google Calendar
+- Authentication handling with OAuth2
+
+Things to update:
+- Adjust separation of credentials and business logic for better modularity
+- Change the code to callable methods instead to integrate into main.py as needed
+"""
+
+
+import os.path
+from datetime import datetime, timezone
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+def main():
+    creds = None
+
+    # Token stores user access + refresh tokens
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    # If no valid credentials, login
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    service = build("calendar", "v3", credentials=creds)
+
+    now = datetime.now(timezone.utc).isoformat()
+    print("Getting upcoming events...")
+
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+
+    events = events_result.get("items", [])
+
+    if not events:
+        print("No upcoming events found.")
+        return
+
+    for event in events:
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        print(start, "-", event["summary"])
+
+if __name__ == "__main__":
+    main()
