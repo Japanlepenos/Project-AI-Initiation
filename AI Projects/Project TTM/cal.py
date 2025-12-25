c3818de193fe8efcef2d34a03bcaabf168677cfb
japanlepenos <j.choladhorn@gmail.com>
2025-12-25 15:56:51 +0700

    More updates

---
+++ AI Projects/Project TTM/cal.py
@@ -0,0 +1,6 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
+import os
+from datetime import datetime
